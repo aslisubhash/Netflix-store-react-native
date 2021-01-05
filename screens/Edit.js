a70f043d33f4e31636fcb54500f47d62f00b3ce9
Subhash Chaudhary <myearning4me@gmail.com>
2021-01-05 19:21:01 +0530

    final update

--- screens/Edit.js
+++ screens/Edit.js
@@ -1,10 +1,80 @@
-import React from "react";
-import {Text,StyleSheet} from "react-native";
+import React, { useState, useEffect } from "react";
 
-const Edit = () => {
-    return (
-        <Text>Edit js</Text>
-    )
+import {Fab, Spinner, Text, List, ListItem, Left, Form, Item, Input, Button, Icon, Body, Right, CheckBox,Title, H1, Subtitle, Container} from "native-base";
+import {StyleSheet}  from "react-native";
+import Snackbar from 'react-native-snackbar';
+import AsyncStorage from "@react-native-community/async-storage";
+import { ScrollView } from "react-native-gesture-handler";
+
+const Edit = ({navigation, route}) => {
+  const [name, setName] = useState("")
+  const [totalNoSeason, setTotalNoSeason] = useState("")
+  const [id, setId] = useState(null)
+
+  const update = async () => {
+    try {
+      if (!name || !totalNoSeason) {
+        return Snackbar.show({
+          text: 'Please Add Both filed...',
+          duration: Snackbar.LENGTH_SHORT,
+        })}
+        const seasontoUpdate = {
+          id,
+          name,
+          totalNoSeason,
+          isWatched: false
+        }
+        const storedValue = await AsyncStorage.getItem("@season_list")
+        const list = await JSON.parse(storedValue)
+
+        list.map((singleSeason)=>{
+          if (singleSeason.id ==id) {
+            singleSeason.name = name
+            singleSeason.totalNoSeason = totalNoSeason
+          }
+          return singleSeason;
+        })
+        await AsyncStorage.setItem("@season_list", JSON.stringify(list))
+        
+        navigation.navigate("Home")
+        
+      
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(()=>{
+    const {season} = route.params
+    const{id, name, totalNoSeason} = season
+
+    setId(id)
+    setName(name)
+    setTotalNoSeason(totalNoSeason)
+  },[])
+
+  return (
+    <Container style={styles.container}>
+      <ScrollView contentContainerStyle={{flexGrow:1}}>
+        <H1 style={styles.heading}>Add to watch list</H1>
+        <Form>
+          <Item rounded style={styles.formItem}>
+            <Input placeholder="Season Name" value={name} onChangeText={(text)=>setName(text)} style={{color:"#eee"}}/>
+          </Item>
+        </Form>
+        <Form>
+          <Item rounded style={styles.formItem}>
+            <Input placeholder="Total number of seasons" value={totalNoSeason} onChangeText={(text)=>setTotalNoSeason(text)} style={{color:"#eee"}}/>
+          </Item>
+        </Form>
+        <Button rounded block
+        onPress ={update}
+        >
+          <Text style={{color:"#eee"}}>Update</Text>
+        </Button>
+      </ScrollView>
+    </Container>
+)
 }
 
 export default Edit;
--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,106 @@
-import React from "react";
-import {Text, StyleSheet, ScrollView} from "react-native";
-import {Fab, Icon} from "native-base"
+import React, {useState, useEffect} from "react";
+import { StyleSheet, ScrollView} from "react-native";
+import {Fab, Spinner, Text, List, ListItem, Left, Button, Icon, Body, Right, CheckBox,Title, H1, Subtitle, Container} from "native-base"
+import AsyncStorage from '@react-native-community/async-storage';
+
+import { useIsFocused } from "@react-navigation/native";
+
 
 const Home = ({navigation, route}) => {
+  const [listOfSeasons, setListOfSeasons] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  const isFocused = useIsFocused()
+
+  const getList = async () => {
+    setLoading (true)
+
+    const storedValue = await AsyncStorage.getItem("@season_list")
+    if (!storedValue) {
+      setListOfSeasons([])
+    }
+
+    const list = JSON.parse(storedValue)
+    setListOfSeasons(list)
+    setLoading(false)
+  }
+
+  const deleteSeason = async (id) => {
+    const newList = await listOfSeasons.filter((list)=>list.id !==id)
+    await AsyncStorage.setItem("@season_list", JSON.stringify(newList))
+    setListOfSeasons(newList)
+  }
+
+  const markComplete = async (id) => {
+    const newArr = listOfSeasons.map((list)=>{
+      if (list.id == id) {
+        list.isWatched = !list.isWatched
+      }
+      return list
+    })
+
+    await AsyncStorage.setItem("@season_list", JSON.stringify(newArr))
+    setListOfSeasons(newArr)
+  }
+
+  useEffect(()=>{
+    getList()
+  },[isFocused])
+
+  if (loading) {
+    return <Container style={styles.container}>
+      <Spinner color="#00b7c2"/>
+    </Container>
+  }
     return (
        <ScrollView contentContainerStyle={styles.container}>
-         <Text>List of seasons goes here..</Text>
+         {listOfSeasons.length === 0 ? (
+         <Container  style={styles.container}>
+           <H1 style={styles.heading}>
+             Watchlist is empty. Please add a season.
+           </H1>
+         </Container>
+         )
+         :
+         (
+           <>
+           <H1 style={styles.heading}>Next series to watch</H1>
+           <List>
+             {listOfSeasons.map((season)=>(
+               <ListItem key={season.id} style={styles.listItem} noBorder>
+               <Left>
+                 <Button style={styles.actionButton} danger
+                 onPress={()=>deleteSeason(season.id)}
+                 >
+                   <Icon name="trash" active>
+                   </Icon>
+                 </Button>
+                 <Button style={styles.actionButton}
+                 onPress={()=>{
+                   navigation.navigate("Edit",{season})
+                 }}
+                 >
+                   <Icon name="edit" type="Feather" active>
+                   </Icon>
+                 </Button>
+               </Left>
+                <Body>
+                  <Title style={styles.seasonName}>{season.name}</Title>
+                  <Text note>{season.totalNoSeason} Seasons to watch</Text>
+                </Body>
+                <Right>
+                <CheckBox
+                checked={season.isWatched}
+                onPress={()=>markComplete(season.id)}
+                />
+                </Right>
+             </ListItem>
+             ))}
+           </List>
+           </>
+         )}
+
+
          <Fab
          style={{backgroundColor:"#5067ff"}}
          position = "bottomRight"
